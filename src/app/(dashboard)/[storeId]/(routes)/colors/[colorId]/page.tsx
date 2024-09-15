fb1a5cfc911c3db4438efb477a19bb0dd8491e44
harshit Mishra <miananya2@gmail.com>
2024-09-16 03:29:41 +0530

    Color Schema

---
+++ src/app/(dashboard)/[storeId]/(routes)/colors/[colorId]/page.tsx
@@ -0,0 +1,20 @@
+import { db } from "@/lib/db";
+import { SizeForm } from "./_components/SizeForm";
+
+const SizePage = async ({ params }: { params: { sizeId: string } }) => {
+  const sizes = await db.size.findUnique({
+    where: {
+      id: params.sizeId,
+    },
+  });
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <SizeForm initialData={sizes} />
+      </div>
+    </div>
+  );
+};
+
+export default SizePage;
---
+++ src/app/(dashboard)/[storeId]/(routes)/colors/page.tsx
@@ -0,0 +1,32 @@
+import { db } from "@/lib/db";
+import { SizeClient } from "./_components/client";
+import { SizeColumn } from "./_components/Columns";
+import { format } from "date-fns";
+
+const SizePage = async ({ params }: { params: { storeId: string } }) => {
+  const sizes = await db.size.findMany({
+    where: {
+      storeId: params.storeId,
+    },
+    orderBy: {
+      createdAt: "desc",
+    },
+  });
+
+  const formattedBillboards: SizeColumn[] = sizes.map((item) => ({
+    id: item.id,
+    name: item.name,
+    value: item.value,
+    createdAt: format(item.createdAt, "MMMM do, yyyy"),
+  }));
+
+  return (
+    <div className="flex-col">
+      <div className="flex-1 space-y-4 p-8 pt-6">
+        <SizeClient data={formattedBillboards} />
+      </div>
+    </div>
+  );
+};
+
+export default SizePage;
